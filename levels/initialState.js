cc076ff7d5abc98716f529a0173aeac63391976d
nirradian <nirradi@gmail.com>
2018-06-10 22:22:32 +0300

    minor things
    added blink text
    output gets an array.
    initial state is prettier.

--- levels/initialState.js
+++ levels/initialState.js
@@ -1,8 +1,10 @@
 export default {
-    output: [],
+    output: ["Sparrownet terminal v4.1.1", "Device Hardware pc0-510551 detected. ", "All systems nominal", "You are logged in as guest."],
     availableCommands: {
         '?' : 'help',
         'help': 'help',
+        'login': 'login', 
+        'quit': 'quit',
     },
     terminal: {
     prompt: '>>> ',
--- redux/outputReducer.js
+++ redux/outputReducer.js
@@ -5,8 +5,11 @@ export default (state, action) => {
 	if (state == null)
 		return [];
 		
-	if (action.output) 
-		return [...state, action.output];
+	if (action.output)  {
+		action.output = Array.isArray(action.output) ? action.output : [action.output];
+		return [...state, ...action.output];
+	}
+		
 	else
 		return state;
 	
